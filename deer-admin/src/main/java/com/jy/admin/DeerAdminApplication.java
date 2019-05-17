15320a49bd6b167e662d92ed0d54cd7937fd552e
bossjim <17521335991@163.com>
2019-05-17 19:40:35 +0800

    deer第一次提交，搭建deer的springboot多模块架构

---
+++ deer-admin/src/main/java/com/jy/admin/DeerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeerAdminApplication.class, args);
+    }
+
+}
---
+++ deer-core/src/main/java/com/jy/core/DeerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jy.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeerCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeerCoreApplication.class, args);
+    }
+
+}
